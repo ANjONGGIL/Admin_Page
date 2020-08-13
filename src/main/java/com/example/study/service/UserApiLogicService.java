bd03f9e92b2fdc984b4e166032897f22380aec12
ANGONGIL <qws458@naver.com>
2020-08-14 00:25:23 +0900

    User CRUD 작성

---
+++ src/main/java/com/example/study/service/UserApiLogicService.java
@@ -0,0 +1,99 @@
+package com.example.study.service;
+
+import com.example.study.ifs.CrudInterface;
+import com.example.study.model.entity.User;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.UserApiRequest;
+import com.example.study.model.network.response.UserApiResponse;
+import com.example.study.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class UserApiLogicService implements CrudInterface<UserApiRequest, UserApiResponse> {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public Header<UserApiResponse> create(Header<UserApiRequest> request) {
+
+        UserApiRequest userApiRequest = request.getData();
+
+        User user = User.builder()
+                .account(userApiRequest.getAccount())
+                .password(userApiRequest.getPassword())
+                .status("REGISTERED")
+                .phoneNumber(userApiRequest.getPhoneNumber())
+                .email(userApiRequest.getEmail())
+                .registeredAt(LocalDateTime.now())
+                .build();
+        User newUser = userRepository.save(user);
+
+        return response(newUser);
+    }
+
+    @Override
+    public Header<UserApiResponse> read(Long id) {
+
+        return userRepository.findById(id)
+                .map(user -> response(user))
+                .orElseGet(
+                        ()->Header.ERROR("데이터 없음")
+                );
+
+    }
+
+    @Override
+    public Header<UserApiResponse> update(Header<UserApiRequest> request) {
+
+        UserApiRequest userApiRequest = request.getData();
+
+        Optional<User> optional = userRepository.findById(userApiRequest.getId());
+
+        optional.map(user -> {
+            user.setAccount(user.getAccount())
+                    .setPassword(user.getPassword())
+                    .setPhoneNumber(user.getPhoneNumber())
+                    .setStatus(user.getStatus())
+                    .setEmail(user.getEmail())
+                    .setUnregisteredAt(user.getUnregisteredAt())
+                    .setRegisteredAt(user.getRegisteredAt());
+            return user;
+        }).map(user -> userRepository.save(user))
+                .map(updateUser->response(updateUser))
+                .orElseGet(()->Header.ERROR("데이터 없음"));
+
+
+        return null; //TODO: 에러 해결 해야됨
+    }
+
+    @Override
+    public Header delete(Long id) {
+
+        Optional<User> optional = userRepository.findById(id);
+
+        optional.map(user -> {
+            userRepository.delete(user);
+            return Header.OK();
+        }).orElseGet(()->Header.ERROR("데이터 없음"));
+        return null;
+    }
+
+    private Header<UserApiResponse> response(User user){
+        UserApiResponse userApiResponse = UserApiResponse.builder()
+                .id(user.getId())
+                .status(user.getStatus())
+                .account(user.getAccount())
+                .password(user.getPassword())
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .registeredAt(user.getRegisteredAt())
+                .unregisteredAt(user.getUnregisteredAt())
+                .build();
+
+        return Header.OK(userApiResponse);
+    }
+}
